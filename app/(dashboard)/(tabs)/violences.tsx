992ff0514fc4ddf13398bb1c505678a93b96add2
Frank14b <franckfontcha@gmail.com>
2024-07-28 00:17:34 +0400

    update slidein animation for tabs

--- app/(dashboard)/(tabs)/violences.tsx
+++ app/(dashboard)/(tabs)/violences.tsx
@@ -6,16 +6,16 @@ import {
   ThemedText,
   ThemedView,
 } from "@/components";
-import { FlatList, Image, StyleSheet } from "react-native";
+import { Image, StyleSheet } from "react-native";
 import { useMemo, useState } from "react";
 import { ViolenceForm } from "@/components/dashboard";
 import { useTabNavigationContext } from "@/contexts/TabNavigationContext";
 import { useViolences } from "@/hooks";
+import { Colors } from "@/constants";
 
 export default function ViolenceScreen() {
   //
   const { slidePosition } = useTabNavigationContext();
-
   const { getViolences } = useViolences();
 
   const [openForm, setOpenForm] = useState<boolean>(false);
@@ -23,22 +23,40 @@ export default function ViolenceScreen() {
   const handleCloseForm = () => setOpenForm(false);
 
   const violences = useMemo(() => {
+    //
     if (getViolences.isLoading) return <ThemedText>Loading...</ThemedText>;
     if (!getViolences.data?.status)
       return <ThemedText>Violence no found</ThemedText>;
 
-    const formattedData = getViolences.data.data?.data.map((item) => {
-      return {
-        title: item.details,
-      };
+    return getViolences.data.data?.data.map((item) => {
+      return (
+        <ThemedView
+          lightColor="#eee"
+          darkColor={Colors.secondaryDark}
+          style={[styles.itemBox]}
+        >
+          <Image
+            source={require("@/assets/images/parallax/violence.avif")}
+            style={styles.itemUserPhoto}
+          />
+          <ThemedView style={{ backgroundColor: "transparent", flex: 1 }}>
+            <ThemedText style={styles.itemUserName}>
+              {item.users.firstname} {item.users.lastname}
+            </ThemedText>
+            <ThemedText>{item.nature}</ThemedText>
+            <ThemedText type="small">
+              {item.ref} | {new Date(`${item.created_at}`).toDateString()}
+            </ThemedText>
+            <ThemedText type="small" style={styles.itemFlag}>
+              {item.flags.name}
+            </ThemedText>
+            <ThemedText type="small" style={styles.itemType}>
+              Type: {item.types.name}
+            </ThemedText>
+          </ThemedView>
+        </ThemedView>
+      );
     });
-
-    return (
-      <FlatList
-        data={formattedData}
-        renderItem={({ item }) => <ThemedText>{item.title}</ThemedText>}
-      ></FlatList>
-    );
   }, [getViolences.data]);
 
   return (
@@ -58,7 +76,9 @@ export default function ViolenceScreen() {
             <ThemedText type="title">Violences</ThemedText>
           </ThemedView>
 
-          <ThemedView style={styles.container}>{violences}</ThemedView>
+          <ThemedView style={{ flex: 1, padding: 10, gap: 10 }}>
+            {violences}
+          </ThemedView>
         </ParallaxScrollView>
       </AnimateSlideInView>
 
@@ -95,4 +115,27 @@ const styles = StyleSheet.create({
     left: 0,
     position: "absolute",
   },
+  itemBox: {
+    flex: 1,
+    flexDirection: "row",
+    borderRadius: 10,
+    padding: 10,
+    gap: 10,
+  },
+  itemUserPhoto: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    objectFit: "cover",
+  },
+  itemUserName: {
+    textTransform: "capitalize",
+    fontWeight: "700",
+  },
+  itemFlag: {
+    position: "absolute",
+    right: 5,
+    top: 0,
+  },
+  itemType: {},
 });
--- types/dashboard/violences.type.ts
+++ types/dashboard/violences.type.ts
@@ -1,7 +1,36 @@
 export interface ResultViolenceDto {
-    details: string;
+  id: number;
+  ref: string;
+  details: string;
+  created_at: Date;
+  date_occured: Date;
+  users: ViolenceUserDto;
+  agent: ViolenceUserDto;
+  created_by: ViolenceUserDto;
+  status: boolean;
+  nature: string;
+  types: ViolenceTypeDto;
+  flags: ViolenceFlagDto;
 }
 
 export type CreateViolenceDto = {
-    details: string;
-}
+  details: string;
+  date_occured: Date;
+  nature: string;
+};
+
+export type ViolenceUserDto = {
+  id: number;
+  firstname: string;
+  lastname: string;
+};
+
+export type ViolenceTypeDto = {
+  id: number;
+  name: string;
+};
+
+export type ViolenceFlagDto = {
+  id: number;
+  name: string;
+};
